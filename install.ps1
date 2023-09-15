ac2dd4f11bfe46dc0dd1dd8af44f26ca41e797e2
Roy <slocomber@hotmail.com>
2023-09-15 13:00:24 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = 'TLS12'
 New-Item -ItemType Directory -Path "C:\ExchangeInstall"
-Invoke-WebRequest -Uri "https://dot.net/v1/dotnet-install.ps1" -OutFile "C:\ExchangeInstall\dotnet-install.ps1"
+#Invoke-WebRequest -Uri "https://dot.net/v1/dotnet-install.ps1" -OutFile "C:\ExchangeInstall\dotnet-install.ps1"
 #iex ((New-Object System.Net.WebClient).DownloadString('https://dot.net/v1/dotnet-install.ps1'))
-#choco install notepadplusplus -y
-#https://dot.net/v1/dotnet-install.ps1
+Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe" -OutFile "C:\ExchangeInstall\NDP48-x86-x64-AllOS-ENU.exe"
+Start-Process "C:\ExchangeInstall\NDP48-x86-x64-AllOS-ENU.exe" -ArgumentList "Setup /quiet /AcceptEULA /norestart /log c:\ExchangeInstall\" -Wait
